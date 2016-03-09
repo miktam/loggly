5a6be2217aeede1ab994f477090d1a54c0d12be4
joaopiopedreira <joaopiopedreira@gmail.com>
2016-03-09 16:38:59 +0000

    Use match() in meteor methods to be able to use the "audit-argument-checks" MDG package

--- logglyMeteorMethods.js
+++ logglyMeteorMethods.js
@@ -12,26 +12,41 @@ var loggerSet = function () {
 
 Meteor.methods({
   logglyLog: function(param, tag) {
+    check(param,Object);
+    check(tag,Match.Optional([String]));
+
     if (loggerSet()){
       Logger.log(param, tag);
     }
   },
   logglyTrace: function(param, tag) {
+    check(param,Object);
+    check(tag,Match.Optional([String]));
+
     if (loggerSet()){
       Logger.trace(param, tag);
     }
   },
   logglyInfo: function(param, tag) {
+    check(param,Object);
+    check(tag,Match.Optional([String]));
+
     if (loggerSet()){
       Logger.info(param, tag);
     }
   },
   logglyWarn:  function(param, tag) {
+    check(param,Object);
+    check(tag,Match.Optional([String]));
+
     if (loggerSet()){
       Logger.warn(param, tag);
     }
   },
   logglyError:  function(param, tag) {
+    check(param,Object);
+    check(tag,Match.Optional([String]));
+
     if (loggerSet()) {
       Logger.error(param, tag);
     }
