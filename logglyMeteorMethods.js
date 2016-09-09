713ea0edfb637180cf71c9ed0cd2d70a69128ad5
Maggi Trymbill <maggi@trymbill.is>
2016-09-09 11:28:23 -0700

    Added string to allowed params
    This fixes #4

--- logglyMeteorMethods.js
+++ logglyMeteorMethods.js
@@ -1,31 +1,31 @@
 Meteor.methods({
   logglyLog: function(param, tag) {
-    check(param,Object);
     check(tag,Match.OneOf([String],null));
+    check(param, Match.OneOf(String, Object));
 
     Logger.log(param, tag);
   },
   logglyTrace: function(param, tag) {
-    check(param,Object);
     check(tag,Match.OneOf([String],null));
+    check(param, Match.OneOf(String, Object));
 
     Logger.trace(param, tag);
   },
   logglyInfo: function(param, tag) {
-    check(param,Object);
     check(tag,Match.OneOf([String],null));
+    check(param, Match.OneOf(String, Object));
 
     Logger.info(param, tag);
   },
   logglyWarn:  function(param, tag) {
-    check(param,Object);
     check(tag,Match.OneOf([String],null));
+    check(param, Match.OneOf(String, Object));
 
     Logger.warn(param, tag);
   },
   logglyError:  function(param, tag) {
-    check(param,Object);
     check(tag,Match.OneOf([String],null));
+    check(param, Match.OneOf(String, Object));
 
     Logger.error(param, tag);
   }
