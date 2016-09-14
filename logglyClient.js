a57c314d908ab2c9fdfe72a2f2a1aff9107dce91
Maggi Trymbill <maggi@trymbill.is>
2016-09-14 14:44:34 -0700

    On development, spit out log to console
    When developing it's useful to see the logs being generated in the
    console. Might want to add a flag for this in settings.json later on

--- logglyClient.js
+++ logglyClient.js
@@ -2,20 +2,25 @@ Logger = {};
 
 Logger.log = function (param, tag) {
   Meteor.call('logglyLog', param, tag, function() {});
+  if (Meteor.isDevelopment) console.log(param);
 };
 
 Logger.trace = function (param) {
   Meteor.call('logglyTrace', param, function() {});
+  if (Meteor.isDevelopment) console.trace(param);
 };
 
 Logger.info = function (param) {
   Meteor.call('logglyInfo', param, function() {});
+  if (Meteor.isDevelopment) console.info(param);
 };
 
 Logger.warn = function (param) {
   Meteor.call('logglyWarn', param, function() {});
+  if (Meteor.isDevelopment) console.warn(param);
 };
 
 Logger.error = function (param) {
   Meteor.call('logglyError', param, function() {});
+  if (Meteor.isDevelopment) console.error(param);
 };
